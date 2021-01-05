4cfed54021e5f582430e1a169422e96db7dca52c
nir <nirp010@gmail.com>
2021-01-05 20:10:13 +0200

    כשלון מכוון

--- app/src/androidTest/java/app/helloworld/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/app/helloworld/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("app.helloworld", appContext.getPackageName());
+        assertEquals("app.helloworld2", appContext.getPackageName());
     }
 }
